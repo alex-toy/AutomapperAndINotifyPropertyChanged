0d8f09783607ec2bc7a6aa365bb25364ed4619e1
alex-toy <alexei.80@hotmail.fr>
2023-02-26 14:26:25 +0100

    init

---
+++ INPCApp/INPC/PersonModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace INPC
+{
+    public class PersonModel : ObservableObject
+    {
+        private string _name;
+
+        public PersonModel()
+        {
+            Task.Run(() => {
+                while (true)
+                {
+                    Random r = new Random();
+                    Name = r.Next(1, 1000).ToString();
+                    Debug.WriteLine($"Name : {Name}");
+                    Thread.Sleep(500);
+                }
+            });
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
